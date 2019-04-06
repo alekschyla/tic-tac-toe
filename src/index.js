59d59d90904346c3a91ef90edb87c6fd616ff3f6
Aleksandra Chyła <aleks.chyla@gmail.com>
2019-04-06 13:28:46 +0200

    Implementing Time Travel

--- src/index.js
+++ src/index.js
@@ -52,7 +52,7 @@ class Game extends React.Component {
     };
 
     handleClick(i) {
-        const history = this.state.history;
+        const history = this.state.history.slice(0, this.state.stepNumber + 1);
         const current = history[history.length - 1];
         const squares = current.squares.slice();
         if (calculateWinner(squares) || squares[i]) return;
@@ -61,6 +61,7 @@ class Game extends React.Component {
             history: history.concat([{
                 squares: squares,
             }]),
+            stepNumber: history.length,
             xIsNext: !this.state.xIsNext,
         })
     }
@@ -74,7 +75,7 @@ class Game extends React.Component {
 
     render() {
         const history = this.state.history;
-        const current = history[history.length - 1];
+        const current = history[this.state.stepNumber];
         const winner = calculateWinner(current.squares);
 
         const moves = history.map((step, move) => {
